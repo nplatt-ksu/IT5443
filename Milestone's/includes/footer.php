8849f192ef0c96a48dc65a14406e43b23c3f8953
Nicholas Platt <nplatt2@students.kennesaw.edu>
2026-07-08 23:24:56 -0400

    Update footer.php

---
+++ Milestone's/includes/footer.php
@@ -0,0 +1,9 @@
+</main>
+  <footer>
+    <p>This website is a student class project for IT 5443 Web Technologies at Kennesaw State University. It is not an official KSU website. For official information about the KSU Interdisciplinary STEM building / Robin and Doug Shore Innovation Center, please visit: <a href="https://www.kennesaw.edu/news/stories/2024/ksu-breaks-ground-interdisciplinary-stem-building.php">KSU official news page about the Interdisciplinary STEM building</a></p>
+  </footer>
+<?php if (isset($pageScript)) { ?>
+  <script src="<?php echo $pageScript; ?>"></script>
+<?php } ?>
+</body>
+</html>
