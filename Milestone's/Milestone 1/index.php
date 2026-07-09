d977180a7c8a4f10f60ec37d769a21510fa2b771
Nicholas Platt <nplatt2@students.kennesaw.edu>
2026-07-08 23:24:16 -0400

    Create index.php

---
+++ Milestone's/Milestone 1/index.php
@@ -0,0 +1,29 @@
+<?php
+$pageTitle = "Home";
+include '../includes/header.php';
+?>
+    <h1>Robin and Doug Shore Innovation Center</h1>
+    <img class="building-image" src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/Robin%20and%20Doug%20Shore%20Innovation%20Center-exterior.jpg?raw=true" alt="Robin and Doug Shore Innovation Center building">
+    <p class="description">
+      The Robin and Doug Shore Innovation Center is a 70,220-square-foot interdisciplinary research and
+      instructional facility on Kennesaw State University's Marietta Campus designed to support faculty-led
+      research and teaching.
+    </p>
+    <div class="two-columns">
+      <div class="column">
+        <h2>Featured Spaces</h2>
+        <ul>
+          <li>Wet and Dry Labs</li>
+          <li>KSU's First Clean Room</li>
+          <li>Cybersecurity and Advanced Computing Labs</li>
+        </ul>
+      </div>
+      <div class="column">
+        <h2>Announcements</h2>
+        <ul>
+          <li><strong>Grand Opening:</strong> September 15, 2026</li>
+          <li><strong>Open House:</strong> October 1, 2026</li>
+        </ul>
+      </div>
+    </div>
+<?php include '../includes/footer.php'; ?>
