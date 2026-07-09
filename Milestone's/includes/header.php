f19524ec413ee138f89654c2f19b886779081ab4
Nicholas Platt <nplatt2@students.kennesaw.edu>
2026-07-08 23:24:11 -0400

    Create header.php

---
+++ Milestone's/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?php echo $pageTitle; ?> - Robin and Doug Shore Innovation Center</title>
+  <link rel="stylesheet" href="../Milestone 1/style.css">
+</head>
+<body>
+  <nav>
+    <a href="https://campus.kennesaw.edu/colleges-departments/ccse/index.php"><img src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/kennesaw_state_owls_logo_secondary.png?raw=true" alt="KSU Owl Logo"></a>
+    <a href="../Milestone 1/index.php">Home</a>
+    <a href="../Milestone 1/facilities.php">Facilities</a>
+    <a href="../Milestone 1/resources.php">Resources</a>
+    <a href="../Milestone 2/eligibility-evaluator.php">Student Eligibility Evaluator</a>
+    <a href="../Milestone 4/research-assistant-application.php">Research Assistant Application</a>
+  </nav>
+  <main>
