eb3349c804af1fd1c8829ca3d128f588edd40ffb
Nicholas Platt <nplatt2@students.kennesaw.edu>
2026-07-08 23:43:46 -0400

    Create resources.php

---
+++ Milestones/Milestone 1/resources.php
@@ -0,0 +1,35 @@
+<?php
+$pageTitle = "Resources";
+include '../includes/header.php';
+?>
+    <h1>Resources</h1>
+    <div class="resources-row">
+      <div class="resources-text">
+        <h2>Building Contacts</h2>
+        <table class="contacts-table">
+          <tr>
+            <th>Role</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          <tr>
+            <td>Lab Manager</td>
+            <td>Jane Doe</td>
+            <td>jdoe@ksu.edu</td>
+          </tr>
+          <tr>
+            <td>Building Manager</td>
+            <td>John Smith</td>
+            <td>jsmith@ksu.edu</td>
+          </tr>
+        </table>
+
+        <h2>Building Address</h2>
+        <p>970 Technology Way, Marietta, GA 30060 Ste. 2513</p>
+      </div>
+
+      <div class="resources-image">
+        <img src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/outside-building.jpg?raw=true" alt="Students outside the Innovation Center">
+      </div>
+    </div>
+<?php include '../includes/footer.php'; ?>
