a87857d54cbb7da55d4a41d1fdb3387ea74d4825
Nicholas Platt <nplatt2@students.kennesaw.edu>
2026-07-08 23:43:42 -0400

    Create facilities.php

---
+++ Milestones/Milestone 1/facilities.php
@@ -0,0 +1,34 @@
+<?php
+$pageTitle = "Facilities";
+include '../includes/header.php';
+?>
+    <h1>Facilities</h1>
+    <p class="description">
+      The Robin and Doug Shore Innovation Center has several cutting edge facilites that faculty can utilize.
+    </p>
+
+    <div class="facilities-row">
+      <div class="facility">
+        <img src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/cleanroom.png?raw=true" alt="Example of the clean room in the Innovation Center">
+        <h2>Clean Room</h2>
+        <p>The Innovation Center has KSU's first-ever clean room that faculty can use to advance their research. The clean room is equipped with state-of-the-art filtration systems and climate control to ensure optimal conditions for sensitive research.</p>
+      </div>
+
+      <div class="facility">
+        <img src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/research-lab.jpg?raw=true" alt="example of one of the research labs in the Innovation Center">
+        <h2>Numerous Labs</h2>
+        <p>The Innovation Center has labs on all three floors of the building.</p>
+        <ul class="facility-list">
+          <li><strong>Floor One:</strong> Metal Lab for Welding, Maker Space Lab</li>
+          <li><strong>Floor Two:</strong> Dry Lab, Cyber Lab</li>
+          <li><strong>Floor Three:</strong> Wet Lab, Teaching Labs</li>
+        </ul>
+      </div>
+
+      <div class="facility">
+        <img src="https://github.com/nplatt-ksu/IT5443/blob/main/Milestones/Milestone%201/Images/lecture-room.jpg?raw=true" alt="Example of one of the classrooms in the Innovation Center">
+        <h2>Classrooms</h2>
+        <p>The Innovation Center also has two classrooms (one 120-student and one 60-student). Each classroom is equipped with the latest technology to enhance the learning experience. The classrooms can also be reserved for events and meetings.</p>
+      </div>
+    </div>
+<?php include '../includes/footer.php'; ?>
